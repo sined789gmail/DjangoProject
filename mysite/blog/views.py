4fe630e56777607bcdaeb753f1c1366c38c1343e
Denis Malorodov <denis.malod@mail.ru>
2025-09-10 15:21:13 +1000

    Сделать представление списка постов классом PostListView

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
 def post_detail(request, year, month, day, post):
@@ -32,3 +33,13 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
 
     return render(request, "blog/post/list.html", {"posts": posts})
+
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'

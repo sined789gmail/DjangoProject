9bfd3be7e7fbadcecc6e814759570dba1a1f9d6e
Denis Malorodov <denis.malod@mail.ru>
2025-09-12 06:58:43 +1000

    Добавить модель Тегов

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
 
 
 class PublishedManager(models.Manager):
@@ -30,6 +32,9 @@ class Post(models.Model):
 
     objects = models.Manager()  # менеджер, применяемый по умолчанию
     published = PublishedManager()  # конкретно-прикладной менеджер
+
+    tags = TaggableManager()
+
 
     class Meta:
         ordering = ["-publish"]
--- temp.py
+++ temp.py
@@ -1,28 +1,4 @@
-Post.objects.create(title='One more post',
-                    slug='one-more-post',
-                    body='Post body.',
-                    author=user)
-
-
-from blog.models import Post
-user = User.objects.get(username='admin')
-post = Post(title='Another post',
-            slug='another-post',
-            body='Post body.',
-            author=user)
-
-post.title = 'New title'
-post.save()
-
-Post.objects.get()
-all_posts = Post.objects.all()
-
-Post.objects.filter(publish__year=2022)
-Post.objects.filter(publish__year=2022, author__username='admin')
-
-
-Post.objects.filter(publish__year=2022) \
-            .exclude(title__startswith='Why')
-
-from blog.models import Post
-Post.published.filter(title__startswith='Who')
+from taggit.managers import TaggableManager
+class Post(models.Model):
+    # ...
+    tags = TaggableManager()

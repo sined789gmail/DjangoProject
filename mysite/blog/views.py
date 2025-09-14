a692da03b82b1b521e6fe3672c738736812fc4d5
Denis Malorodov <denis.malod@mail.ru>
2025-09-14 23:29:39 +1000

    Добавить отображение схожих постов в представление деталей поста

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,6 +8,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_share(request, post_id):
@@ -48,10 +49,23 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
+
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
@@ -76,7 +90,7 @@ def post_list(request, tag_slug=None):
         # выдать последнюю страницу
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, "blog/post/list.html", {"posts": posts, 'tag': tag})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 class PostListView(ListView):

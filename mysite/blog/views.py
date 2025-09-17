2b7ad578bb7cf2e711d309b899bee1bc133213c9
Denis Malorodov <denis.malod@mail.ru>
2025-09-17 14:56:43 +1000

    Добавить функциональность поиска постов с использованием формы поиска

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,9 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
+
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
@@ -121,4 +123,23 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )

84c9c4102849f79b3dafa719603b4c305f024112
Denis Malorodov <denis.malod@mail.ru>
2025-09-17 16:26:34 +1000

    Добавить улучшенный поиск постов с использованием TrigramSimilarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,7 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 
 from django.core.mail import send_mail
@@ -11,6 +11,8 @@ from django.views.decorators.http import require_POST
 
 from taggit.models import Tag
 from django.db.models import Count
+
+from django.contrib.postgres.search import TrigramSimilarity
 
 
 def post_share(request, post_id):
@@ -130,14 +132,19 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
-    
+
     if "query" in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.published.annotate(
-                search=SearchVector("title", "body"),
-            ).filter(search=query)
+            results = (
+                Post.published.annotate(
+                    similarity=TrigramSimilarity("title", query),
+                )
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
+            )
+
     return render(
         request,
         "blog/post/search.html",
